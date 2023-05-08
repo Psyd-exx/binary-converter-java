91b20f29c0b1f4fa483a2ce19ddbc99d28307501
Sinan <psydexx64@gmail.com>
2023-05-08 01:19:06 +0100

    init: create classes and add user input

---
+++ BinaryConverter.java
@@ -0,0 +1,13 @@
+class BinaryConverter {
+    private String binaryInput;
+
+    public BinaryConverter(String binaryInput) {
+        this.binaryInput = binaryInput;
+    }
+
+    public int convert(){
+        int result = 0;
+        
+        return result;
+    }
+}
---
+++ Main.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main {
+    private static String binaryInput;
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Please input the binary number here: ");
+        binaryInput = scanner.next();
+
+        BinaryConverter convertNew = new BinaryConverter(binaryInput);
+    }   
+}
